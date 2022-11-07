caaafad9df35f0d2169734c34fb9041dd2f778d2
Wuzhiyu <wuzhiyu11@126.com>
2022-11-07 15:16:35 +0800

    first commit

---
+++ 测试2/src/gitTest.java
@@ -0,0 +1,7 @@
+public class gitTest {
+    public static void main(String[] args) {
+        System.out.println("第一次提交");
+        System.out.println("第一次提交");
+        System.out.println("第一次提交");
+    }
+}
